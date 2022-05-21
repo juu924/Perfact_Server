46ae70998bea284e666b080b495a2d71dbffacde
juu924 <osj8528@gmail.com>
2022-05-21 13:37:07 +0900

    장소 스크랩 API

---
+++ src/main/java/com/perfact/server/domain/LikePlace.java
@@ -0,0 +1,39 @@
+package com.perfact.server.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class LikePlace {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   @Column(nullable = false)
+    private Long userId;
+
+   @Column(length = 20, nullable = false)
+    private String name;
+
+   @Column(length = 200)
+    private String addr;
+
+   @Column(length = 50)
+    private String telephone;
+
+   @Builder
+    public LikePlace(Long userId, String name, String addr, String telephone){
+       this.userId = userId;
+       this.name = name;
+       this.addr = addr;
+       this.telephone = telephone;
+   }
+
+
+
+}
